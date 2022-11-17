4925442b3eb75a18a65e55e8e11eeaf259196148
Randy Kim <rhkim1292@gmail.com>
2022-11-16 23:51:20 -0800

    Write tests for and implement the Ship factory fn
    More specifically, the methods within the Ship factory that were tested
    are hit() which should increase the number of 'hits' on the ship it was
    called on.
    
    isSunk() is the other method that should return true when the number of
    hits on the ship are equal to the length of the ship and false otherwise.

--- src/Ship.js
+++ src/Ship.js
@@ -1,3 +1,19 @@
-export default function sum(a, b) {
-    return a + b;
-}
+const Ship = (len) => {
+    const shipLength = len;
+	let hits = 0;
+	const hit = () => {
+		hits++;
+	};
+    const isSunk = () => {
+        return hits === shipLength;
+    };
+	return {
+        get hits() {
+            return hits;
+        },
+        hit,
+        isSunk,
+    };
+};
+
+export default Ship;
--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,5 +1,22 @@
-import sum from './Ship.js';
+import Ship from './Ship.js';
 
-it('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-})
+describe('Ship object functionality', () => {
+	it("hit() increases the number of 'hits' on a Ship", () => {
+        const ship = Ship();
+        ship.hit();
+		expect(ship.hits).toBe(1);
+	});
+    it("isSunk() returns true when a Ship's hits is equal to its length", () => {
+        const shipLength = 3;
+        const ship = Ship(shipLength);
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    });
+    it("isSunk() returns false when a Ship's hits is not equal to its length", () => {
+        const shipLength = 1;
+        const ship = Ship(shipLength);
+        expect(ship.isSunk()).toBe(false);
+    });
+});
