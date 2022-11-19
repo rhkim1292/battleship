1b2edc8e1333048b2301c794107eaaa5b8d8dbcc
Randy Kim <rhkim1292@gmail.com>
2022-11-19 01:02:58 -0800

    Test and begin implementation of placeShip method
    The placeShip method has been tested to be able to place ships on the
    gameboard by occupying space on a Gameboard object's boardArr.
    
    Preventing users from placing ships that would go off the gameboard
    still need to be tested and implemented.

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -2,10 +2,17 @@ import Ship from './Ship.js';
 
 const Gameboard = () => {
     const boardArr = Array.from({ length: 10 }, (e, idxY) => Array.from({ length: 10}, (e, idxX) => [idxX, idxY]));
-    const placeShip = (x, y) => {
-        
+    const valueAt = (x, y) => {
+        return boardArr[y][x];
+    }
+    const placeShip = (ship) => {
+        const shipCoordinates = ship.getCoordinates();
+        for (let i = 0; i < shipCoordinates.length; i++) {
+            boardArr[shipCoordinates[i][1]][shipCoordinates[i][0]] = ship;
+        }
     };
     return {
+        valueAt,
         placeShip,
     }
 }
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -1,9 +1,46 @@
 import Ship from './Ship.js';
 import Gameboard from './Gameboard.js';
 
+describe('valueAt() functionality', () => {
+    it('valueAt(-1, 0) should be undefined', () => {
+        const gameboard = Gameboard();
+        expect(gameboard.valueAt(-1, 0)).toBeUndefined();
+    });
+    it('valueAt(0, 0) should return [0, 0] in a newly initialized Gameboard', () => {
+        const gameboard = Gameboard();
+        expect(gameboard.valueAt(0, 0)).toStrictEqual([0, 0]);
+    });
+});
 describe('placeShip() functionality', () => {
-    it('throws an error when trying to place a ship out of bounds', () => {
-        const gameboard = new Gameboard();
-        expect(gameboard.placeShip(-1, 0)).toThrow();
+	it('throws an error when trying to place a ship out of bounds', () => {
+		const gameboard = Gameboard();
+		expect(() => {
+            const ship = Ship(-1, 0, 1);
+            gameboard.placeShip(ship)
+        }).toThrow();
+	});
+	it('placing a ship at (0, 0) with a length of 1 should make the gameboard return a reference to the ship only at (0, 0)', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(0, 0, 1);
+        gameboard.placeShip(ship);
+        expect(gameboard.valueAt(0, 0)).toStrictEqual(ship);
+        expect(gameboard.valueAt(0, 1)).not.toStrictEqual(ship);
+        expect(gameboard.valueAt(1, 0)).not.toStrictEqual(ship);
+    });
+    it('placing a ship at (0, 0) with a length of 2 and an orientation of \'v\' should make the gameboard return a reference to the ship at both (0, 0) and (0, 1)', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(0, 0, 2);
+        gameboard.placeShip(ship);
+        expect(gameboard.valueAt(0, 0)).toStrictEqual(ship);
+        expect(gameboard.valueAt(0, 1)).toStrictEqual(ship);
+        expect(gameboard.valueAt(1, 0)).not.toStrictEqual(ship);
+    });
+    it('placing a ship at (0, 0) with a length of 2 and an orientation of \'h\' should make the gameboard return a reference to the ship at both (0, 0) and (1, 0)', () => {
+        const gameboard = Gameboard();
+        const ship = Ship(0, 0, 2, 'h');
+        gameboard.placeShip(ship);
+        expect(gameboard.valueAt(0, 0)).toStrictEqual(ship);
+        expect(gameboard.valueAt(0, 1)).not.toStrictEqual(ship);
+        expect(gameboard.valueAt(1, 0)).toStrictEqual(ship);
     });
 });
