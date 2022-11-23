d2bfcba9338b33cd11bc093c7ccdc0bab548f7d2
Randy Kim <rhkim1292@gmail.com>
2022-11-23 00:05:01 -0800

    Setup the css and dom necessary for the game UI
    Placeholder 'p' element was placed where the 10x10 gameboard will go
    for each player.

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -1,12 +1,33 @@
+import './style.css';
+
 const DOMHandler = (() => {
-	const testFn = () => {
-		const h1 = document.createElement('h1');
-		h1.textContent = 'testing!';
-		document.body.appendChild(h1);
+	const GAME_CONTAINER_WIDTH = 800;
+
+	const title = document.createElement('h1');
+	title.textContent = 'Battleship!';
+	const gameContainer = document.createElement('div');
+	gameContainer.classList.add('game-container');
+	const p1Container = document.createElement('div');
+	p1Container.classList.add('p1-container');
+	const p2Container = document.createElement('div');
+	p2Container.classList.add('p2-container');
+	gameContainer.append(p1Container, p2Container);
+	document.body.append(title, gameContainer);
+	const createGameboardElement = (gameboard) => {
+		const placeholder = document.createElement('p');
+		placeholder.textContent = 'Gameboard grid goes here!';
+		return placeholder;
+	};
+	const renderPlayerData = (player, queryString) => {
+		const p1Container = document.querySelector(queryString);
+		const playerBoardHeader = document.createElement('h2');
+		playerBoardHeader.textContent = `${player.name}'s Board`;
+		const playerGameboard = createGameboardElement(player.board);
+		p1Container.append(playerBoardHeader, playerGameboard);
 	};
 	return {
-        testFn,
-    };
+		renderPlayerData,
+	};
 })();
 
 export default DOMHandler;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import Gameboard from './Gameboard.js';
 import DOMHandler from './DOMHandler.js';
 
 const mainGameLoop = (() => {
-    DOMHandler.testFn();
+    const p1Gameboard = Gameboard();
+    const p1 = Player(prompt('Enter your name.'), p1Gameboard);
+    DOMHandler.renderPlayerData(p1, 'div.p1-container');
+    const p2Gameboard = Gameboard();
+    const p2 = Player('CPU', p2Gameboard, true);
+    DOMHandler.renderPlayerData(p2, 'div.p2-container');
 })();
