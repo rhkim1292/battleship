567d6c8aebca67eadba4602ec679fa46d1a1dfcb
Randy Kim <rhkim1292@gmail.com>
2022-11-23 20:41:22 -0800

    Implement rendering of player gameboards

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -1,8 +1,6 @@
 import './style.css';
 
 const DOMHandler = (() => {
-	const GAME_CONTAINER_WIDTH = 800;
-
 	const title = document.createElement('h1');
 	title.textContent = 'Battleship!';
 	const gameContainer = document.createElement('div');
@@ -13,18 +11,38 @@ const DOMHandler = (() => {
 	p2Container.classList.add('p2-container');
 	gameContainer.append(p1Container, p2Container);
 	document.body.append(title, gameContainer);
+
 	const createGameboardElement = (gameboard) => {
-		const placeholder = document.createElement('p');
-		placeholder.textContent = 'Gameboard grid goes here!';
-		return placeholder;
+		// const placeholder = document.createElement('p');
+		// placeholder.textContent = 'Gameboard grid goes here!';
+		// return placeholder;
+		const gameboardElement = document.createElement('div');
+		gameboardElement.classList.add('gameboard');
+
+		for (let y = 0; y < 10; y++) {
+			for (let x = 0; x < 10; x++) {
+				let tile = document.createElement('div');
+				tile.classList.add('board-cell');
+				tile.setAttribute('data-x', `${x}`);
+				tile.setAttribute('data-y', `${y}`);
+				gameboardElement.appendChild(tile);
+			}
+		}
+
+		return gameboardElement;
 	};
+
 	const renderPlayerData = (player, queryString) => {
-		const p1Container = document.querySelector(queryString);
+		const playerContainer = document.querySelector(queryString);
+		const playerContainerColumns = document.createElement('div');
+		playerContainerColumns.classList.add('player-container-cols');
 		const playerBoardHeader = document.createElement('h2');
 		playerBoardHeader.textContent = `${player.name}'s Board`;
 		const playerGameboard = createGameboardElement(player.board);
-		p1Container.append(playerBoardHeader, playerGameboard);
+		playerContainerColumns.append(playerBoardHeader, playerGameboard);
+		playerContainer.appendChild(playerContainerColumns);
 	};
+	
 	return {
 		renderPlayerData,
 	};
